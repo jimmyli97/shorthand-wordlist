7a8042c1e802ff954b7f8c9f0ed39f92f8c0a246
Jimmy Li <jimmy.jiajian.li@gmail.com>
2019-11-19 22:31:57 -0500

    try out 20 chars with no 5 character word

--- gen.pl
+++ gen.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-open my $data, '<', 'words/10k.txt';
+open my $data, '<', 'words/20k_no5char.txt';
 chomp (my @unabbrev = <$data>);
 close $data;
 
@@ -17,18 +17,23 @@ open (my $outh, '>', $out);
 
 my %dict;
 
+# sort abbrev by word length
+@unabbrev = sort {length $a <=> length $b} @unabbrev;
+
 foreach my $unab (@unabbrev) {
 	# remove vowels except for first
 	my $novow = $unab;
+	my $cur = $novow;
 	# use re 'debug';
 	while (1) {
-		my $cur = $novow;
+		my $subbed = ($cur =~  s/^[a-z]\w*?\K[aeiou]//g);
 		if (exists($dict{$cur})) {
 			# repeat, can't remove all vowels
 			print $exceptionh "$cur\t$unab\n";
 			last;
-		} elsif (!($cur =~ s/^[a-z]\w*?\K[aeiou]//g)) {
-			# no more substitutions
+		} elsif (!$subbed){
+			# no more substitutions possible
+			$novow = $cur;
 			last;
 		} else {
 			$novow = $cur;
@@ -43,7 +48,7 @@ foreach my $unab (@unabbrev) {
 
 foreach my $ab (keys %dict) {
 	print $outh "iabbr $ab\t$dict{$ab}\n";
-	print $outh "iabbr ",ucfirst($ab),"\t",ucfirst($dict{$ab}),"\n";
+	# print $outh "iabbr ",ucfirst($ab),"\t",ucfirst($dict{$ab}),"\n";
 }
 
 close $duph;
